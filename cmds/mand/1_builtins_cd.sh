2f0b8620e8bb4cb363eafa587997d42a6c3b673d
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-02-12 17:09:22 +0100

    I have splited the builtins tests

---
+++ cmds/mand/1_builtins_cd.sh
@@ -0,0 +1,165 @@
+# **************************************************************************** #
+#                                   BUILTINS - CD                                   #
+# **************************************************************************** #
+
+### CD ###
+pwd
+cd
+pwd
+
+pwd
+cd ..
+pwd
+
+cd ..
+cd $OLDPWD
+
+pwd
+cd .
+pwd
+
+pwd
+cd "."
+pwd
+
+pwd
+cd /Users
+pwd
+
+pwd
+cd //////
+pwd
+
+pwd
+cd ./././
+pwd
+
+pwd
+cd /
+pwd
+
+pwd
+cd '/////'
+pwd
+
+pwd
+cd "$PWD/file_tests"
+pwd
+
+cd '/////' 2>/dev/null
+
+cd '/etc' 
+
+cd '/var' 
+
+cd "doesntexist" 2>/dev/null 
+
+cd ../../..
+
+cd "wtf" 2>/dev/null 
+
+cd ?
+
+cd +
+
+cd _
+
+cd woof
+
+cd bark bark
+
+cd '/' 
+
+pwd
+cd "doesntexist"
+pwd
+
+pwd
+cd "doesntexist"
+pwd
+
+pwd
+cd ../../..
+pwd
+
+pwd
+cd "wtf"
+pwd
+
+pwd
+cd ..
+cd ..
+pwd
+
+pwd
+cd ?
+pwd
+
+pwd
+cd +
+pwd
+
+pwd
+cd _
+pwd
+
+cd ../../
+cd -
+cd -
+
+pwd
+cd echo
+pwd
+
+pwd
+cd echo echo
+pwd
+
+pwd
+cd '/'
+pwd
+
+pwd
+cd $PWD/file_tests
+pwd
+
+pwd
+cd $OLDPWD/something
+pwd
+
+pwd
+cd ~
+cd - ananas dot jpeg
+pwd
+
+# might need to change the directory by yourself, that supposed to be valid =)
+pwd
+cd includes/ djhwbdhwbd wgdyuhgw jdwjdh wuiydjwh 
+pwd
+
+pwd
+cd ~ asd w dd q asd
+pwd
+
+pwd
+cd ~
+cd -
+pwd
+
+pwd
+cd -
+pwd
+
+pwd
+cd ~
+pwd
+
+# might need to change the directory by yourself, that supposed to be valid =)
+pwd
+cd ../libft
+pwd
+
+# might need to change the directory by yourself, that supposed to be valid =)
+pwd
+cd ../libft ojid iowjiojwoid joijd owjoid jwojd 
+pwd
--- tester.sh
+++ tester.sh
@@ -28,7 +28,23 @@ main() {
 	do
 		case $2 in
 			"builtins" | "b") 
-				TESTFILES+="${RUNDIR}/cmds/mand/1_builtins.sh"
+				if [ "$3" == "echo" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_echo.sh"  # Run echo-specific tests
+				elif [ "$3" == "cd" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_cd.sh"    # Run cd-specific tests
+				elif [ "$3" == "pwd" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_pwd.sh"   # Run pwd-specific tests
+				elif [ "$3" == "export" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_export.sh" # Run export-specific tests
+				elif [ "$3" == "unset" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_unset.sh"  # Run unset-specific tests
+				elif [ "$3" == "env" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_env.sh"    # Run env-specific tests
+				elif [ "$3" == "exit" ]; then
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins_exit.sh"   # Run exit-specific tests
+				else
+					TESTFILES+="${RUNDIR}/cmds/mand/1_builtins.sh"         # Run all builtins tests
+				fi
 				;;
 			"parsing" | "pa") 
 				TESTFILES+=" ${RUNDIR}/cmds/mand/0_compare_parsing.sh"
